a11dfe562a23bc3cca603b7fba2875753bd85acc
Terra Oldham <toldham@hearsaycorp.com>
2015-03-11 17:41:41 -0700

    Chapter 4 Functional Test Added

--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -17,10 +18,20 @@ class NewVisitorTest(unittest.TestCase):
 
 		# She notices the page title and header mention to-do lists
 		self.assertIn('To-Do', self.browser.title)
-		self.fail('Finish the test')
+		header_text = self.browser.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do', header_text)
 	
 		# She is invited to enter a to-do item straight away
-
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		self.assertEqual(
+			inputbox.get_attribute('placeholder'),
+			'Enter a to-do item'
+		)
+		inputbox.send_keys('Buy peacock feather')
+		inputbox.send_keys(Keys.ENTER)
+		
+		self.fail('Finish the test!')
+			
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
 
