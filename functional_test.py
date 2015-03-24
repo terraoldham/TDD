62263206c3efabb84609303a4b3ff4a8ac3882da
Hearsay Training <training@Hearsays-iMac.local>
2015-03-23 18:15:17 -0700

    Add a second line

--- functional_test.py
+++ functional_test.py
@@ -7,11 +7,11 @@ class NewVisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.browser = webdriver.Firefox()
 		self.browser.implicitly_wait(3)
-	
+
 	def tearDown(self):
 		self.browser.quit()
-		
-	def test_can_start_a_list_and_retrieve_it_later(self): 
+
+	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
 		self.browser.get('http://localhost:8000')
@@ -20,31 +20,35 @@ class NewVisitorTest(unittest.TestCase):
 		self.assertIn('To-Do', self.browser.title)
 		header_text = self.browser.find_element_by_tag_name('h1').text
 		self.assertIn('To-Do', header_text)
-	
+
 		# She is invited to enter a to-do item straight away
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		self.assertEqual(
 			inputbox.get_attribute('placeholder'),
 			'Enter a to-do item'
 		)
-		
+
 		#She types "Buy peacock feather" into a text box ('Edith's hobby
 		# is tying fly fishing lures)
 		inputbox.send_keys('Buy peacock feather')
-		
+
 		# When she hits enter, the page updates, and now the pages lists
 		# "1: Buy Peacock Feathers" as an item in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
-		
+
 		table = self.browser.find_element_by_id('id_list_table')
 		rows = table.find_elements_by_tag_name('tr')
-		self.assertTrue(
-			any(row.text == '1: Buy peacock feathers' for row in rows),
-			"New to-do item did not appear in table"
-		)
+		self.assertIn('1: Buy peacock feather', [row.text for row in rows])		
 		
+		inputbox = self.browser.find_element_by_id('id_new_item')
+		inputbox.send_keys('Use peacock feathers to make a fly')
+		inputbox.send_keys(Keys.ENTER)
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn('1: Buy peacock feather', [row.text for row in rows])			
+		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
 		self.fail('Finish the test!')
-			
+
 if __name__ == '__main__':
 	unittest.main(warnings='ignore')
 
