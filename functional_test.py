16ec495299ecb1be4d6ef69b2dbb899ee0c2335d
Hearsay Training <training@Hearsays-iMac.local>
2015-03-23 18:52:21 -0700

    Add new function for the unit tests

--- functional_test.py
+++ functional_test.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
 	def tearDown(self):
 		self.browser.quit()
 
+	def check_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
+
 	def test_can_start_a_list_and_retrieve_it_later(self):
 		# Edith has heard about a cool new online to-do app. She goes
 		# to check out its homepage
@@ -36,17 +41,13 @@ class NewVisitorTest(unittest.TestCase):
 		# "1: Buy Peacock Feathers" as an item in a to-do list table
 		inputbox.send_keys(Keys.ENTER)
 
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feather', [row.text for row in rows])		
+		self.check_for_row_in_list_table('1: Buy peacock feather')
 		
 		inputbox = self.browser.find_element_by_id('id_new_item')
 		inputbox.send_keys('Use peacock feathers to make a fly')
 		inputbox.send_keys(Keys.ENTER)
-		table = self.browser.find_element_by_id('id_list_table')
-		rows = table.find_elements_by_tag_name('tr')
-		self.assertIn('1: Buy peacock feather', [row.text for row in rows])			
-		self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+		self.check_for_row_in_list_table('1: Buy peacock feather')		
+		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')		
 		self.fail('Finish the test!')
 
 if __name__ == '__main__':
