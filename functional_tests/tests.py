8640fc2ffdd020ceff74d3bc85fd1497852ed015
Hearsay Training <training@Hearsays-iMac.local>
2015-04-08 19:27:09 -0700

    New URL view and Template to Display Lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -37,7 +37,7 @@ class NewVisitorTest(LiveServerTestCase):
 
                 #She types "Buy peacock feather" into a text box ('Edith's hobby
                 # is tying fly fishing lures)
-                inputbox.send_keys('Buy peacock feather')
+                inputbox.send_keys('Buy peacock feathers')
 
                 # When she hits enter, the page updates, and now the pages lists
                 # "1: Buy Peacock Feathers" as an item in a to-do list table
@@ -46,7 +46,6 @@ class NewVisitorTest(LiveServerTestCase):
                 self.assertRegex(edith_list_url, '/lists/.+')
                 self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-                self.check_for_row_in_list_table('1: Buy peacock feather')
                 inputbox = self.browser.find_element_by_id('id_new_item')
                 inputbox.send_keys('Use peacock feathers to make a fly')
                 inputbox.send_keys(Keys.ENTER)
@@ -63,7 +62,7 @@ class NewVisitorTest(LiveServerTestCase):
                 self.assertNotIn('Buy peacock feathers', page_text)
                 self.assertNotIn('make a fly', page_text)
 
-                inputbox = self.browser.find_by_id('id_new_item')
+                inputbox = self.browser.find_element_by_id('id_new_item')
                 inputbox.send_keys('Buy milk')
                 inputbox.send_keys(Keys.ENTER)
 
